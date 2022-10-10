73e4f0e9a34d4d677198eb3fe2308ce99dc65503
Danseh <xxfirox123@gmail.com>
2022-10-10 19:26:33 +0200

    tourcms commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'tourcms/tourcms-php' => array(
+            'pretty_version' => 'v3.9.1',
+            'version' => '3.9.1.0',
+            'reference' => '0fcbf92c8ac3ddbbc3baf136e39b96b0d1b76d13',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../tourcms/tourcms-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
